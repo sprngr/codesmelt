95d57e03e5a270b626597412547150c89cff28b3
Michael Springer <mail@michael-springer.net>
2012-05-30 23:20:16 -0400

    Removed unnecessary files from source.

--- php/open_file_1.php
+++
@@ -1,6 +0,0 @@
-<?php
-$the_temp_file=$_FILES['users_file']['tmp_name'];
-$file_contents=file_get_contents($the_temp_file);
-?><body onload='parent.openFileComplete()'><textarea id='the_file'><?php
-echo $file_contents;
-?></textarea></body>
